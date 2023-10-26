03000f14c671d2a583ecdc03a52651be9e6bcc81
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2023-10-27 00:19:47 +0300

    done css

--- Js/hero-modal-menu.js
+++ Js/hero-modal-menu.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttonClose = document.querySelector('.hero-svg-menu-close');
     const modal = document.querySelector('.hero-mobile-menu');
     const backdrop = document.querySelector('.backdrop-hero')
+    const price = document.getElementById('open-modal-deckstop')
 
     buttonOpen.addEventListener('click', openModal);
     buttonClose.addEventListener('click', closeModal);
+    
+    price.addEventListener('click', openModal)
 
     function openModal(){
-        console.log('Модалка открыта')
         modal.classList.remove('is-hidden-hero');
         backdrop.classList.remove('is-hidden-hero');
+        document.body.style.overflow = "hidden"
     }
 
     function closeModal(){
         modal.classList.add('is-hidden-hero');
         backdrop.classList.add('is-hidden-hero');
+        document.body.style.overflow = ''; 
     }
 })
