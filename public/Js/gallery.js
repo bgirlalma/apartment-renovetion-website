2e07c70839c1788359fe3c8bda563095db03349d
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2024-01-09 22:47:23 +0300

    add simplelightbox js

--- public/Js/gallery.js
+++ public/Js/gallery.js
@@ -1,1 +1,36 @@
-import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.esm";
+
+const container = document.querySelector(".gallery");
+
+// Открімуємо линку на зображення
+const imageFolder = "/public/Image/gallery-photo";
+const imageFiles = [
+  "bathroom-grey.jpg",
+  "bathroom.jpg",
+  "entrance-doors.jpg",
+  "figured-ceiling.jpg",
+  "hallway.jpg",
+  "wall-decoration.jpg",
+];
+
+// створюємо HTML розмітку для галереї
+const galleryHtml = imageFiles
+  .map(
+    (filename) =>
+      `<li>
+    <a href="${imageFolder}${filename}">
+    <img src="${imageFolder}${filename}" alt="gallery"/></a></li>`
+  )
+  .join("");
+
+
+container.innerHTML = galleryHtml;
+
+    new SimpleLightbox(".gallery a", {
+      captionDelay: 250,
+      captionsData: "alt",
+      captionPosition: "bottom",
+    }); 
+
+    console.log(galleryHtml)
