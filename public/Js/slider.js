9c86ad7786476bb2db45ab446155e07ae091ffd8
Alina <akulina.alina1999gmail.com>
2024-02-08 14:08:19 +0300

    add swiper

--- public/Js/slider.js
+++ public/Js/slider.js
@@ -1,9 +1,22 @@
-document.getElementById('type-next').onclick = function(){
-let lists = document.querySelectorAll('.item-slide');
-document.getElementById('slide').appendChild(lists[0])
-}
+// Инициализация Swiper
+const swiper = new window.Swiper(".swiper", {
+  // Настройки слайдера
+  direction: "horizontal", // Направление прокрутки слайдов (горизонтальное)
+  loop: true, // Зацикливание слайдов
 
-document.getElementById('type-prev').onclick = function(){
-    let lists = document.querySelectorAll('.item-slide');
-    document.getElementById('slide').prepend(lists[lists.length - 1])
-}
+  // Настройки пагинации (точек внизу слайдера)
+  pagination: {
+    el: ".swiper-pagination", // Элемент, куда вставляются точки пагинации
+  },
+
+  // Настройки навигации (стрелок для переключения слайдов)
+  navigation: {
+    nextEl: ".swiper-button-next", // Элемент "вперед"
+    prevEl: ".swiper-button-prev", // Элемент "назад"
+  },
+
+  // Настройки полосы прокрутки (если необходимо)
+  scrollbar: {
+    el: ".swiper-scrollbar", // Элемент полосы прокрутки
+  },
+});
